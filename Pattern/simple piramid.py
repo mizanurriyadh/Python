c2a5505d6e68f347d1d9813163f5735296c7b111
mizanurriyadh <mizanurriyadh@ymail.com>
2017-06-21 03:00:28 +0600

    Pattern/Simple pyramid.py completed

---
+++ Pattern/simple piramid.py
@@ -0,0 +1,17 @@
+def pypart(n):
+    # outer loop to handle number of rows
+
+    for i in range(0, n):
+
+        # inner loop to handle number of column
+        for j in range(0, i+1):
+
+            # printing stars
+            print("* ", end="")
+
+        # ending line after each row
+        print("\r")
+
+# Driver Code
+n = 5
+pypart(n)
